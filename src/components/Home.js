9d6ca5722b0839a1e6c3c46f0ec3857f4fa13d35
rajregmi98 <rajregmi98@gmail.com>
2022-06-29 19:38:21 -0700

    my_new_project

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div>
+     <h1> send them a smile with happy cards!</h1>
+      <h2>Choose from thousand of things...</h2>
+        </div>
+        );
+}
